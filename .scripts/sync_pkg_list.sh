08e0ef2c254cca4283427045ad555bd9373299c5
Simon Amadeus <simon.joedicke@code.berlin>
2023-11-04 13:04:28 +0100

    feat: add timed package sync

---
+++ .scripts/sync_pkg_list.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Executed by systemd timer/service
+#
+
+# Define the package list paths
+PKGLIST="$HOME/.config/packages/pkglist.txt"
+AURPKGLIST="$HOME/.config/packages/aur_pkglist.txt"
+
+# Define your git directory for dotfiles
+DOTFILES_GIT_DIR="$HOME/.dotfiles"
+GIT_WORK_TREE="$HOME"
+
+# Create the package lists
+pacman -Qqen > "$PKGLIST"
+pacman -Qqem > "$AURPKGLIST"
+
+# Perform git operations using the specific directories
+/usr/bin/git --git-dir="$DOTFILES_GIT_DIR" --work-tree="$GIT_WORK_TREE" add "$PKGLIST" "$AURPKGLIST"
+/usr/bin/git --git-dir="$DOTFILES_GIT_DIR" --work-tree="$GIT_WORK_TREE" diff-index --quiet HEAD || /usr/bin/git --git-dir="$DOTFILES_GIT_DIR" --work-tree="$GIT_WORK_TREE" commit -m "Update package lists"
+
+# Push changes - optional, remove the next line if you don't want to automatically push
+/usr/bin/git --git-dir="$DOTFILES_GIT_DIR" --work-tree="$GIT_WORK_TREE" push
+
