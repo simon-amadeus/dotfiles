b0ef18a7a15c6a090949637f97a08286b4434518
Simon Amadeus <code@simonamadeus.com>
2026-05-12 17:16:10 +0200

    Improve utility scripts

--- .config/sway/scripts/lockman.sh
+++ .config/sway/scripts/lockman.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-# Times the screen off and puts it to background
+# Turn off display while lock screen is showing, then restore on unlock.
 swayidle \
-    timeout  300 'swaymsg "output * power off"' \
-    resume 'swaymsg "output * power on"' &
-# Locks the screen immediately
-swaylock
-# Kills last background task so idle timer doesn't keep running
-kill %%
+    timeout 300 'swaymsg "output * power off"' \
+    resume  'swaymsg "output * power on"' &
+IDLE_PID=$!
+swaylock -f -C ~/.config/swaylock/config
+kill "$IDLE_PID"
